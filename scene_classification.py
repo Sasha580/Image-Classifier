3c82733edcca704c47c8b063807cf1f65dac56f7
aleksandrabobrova <sashabombus@gmail.com>
2025-11-26 00:02:09 +0100

    PreAct Block

--- scene_classification.py
+++ scene_classification.py
@@ -8,6 +8,8 @@ from PIL import Image
 from torchvision import transforms
 from torch.utils.data import DataLoader, Dataset
 
+from torch.optim.lr_scheduler import LinearLR, CosineAnnealingLR, SequentialLR
+
 
 class MiniPlaces(Dataset):
     def __init__(self, root_dir, split, transform=None, label_dict=None):
@@ -68,10 +70,9 @@ class MiniPlaces(Dataset):
             image = self.transform(
                 Image.open(os.path.join(self.root_dir, "images", self.filenames[idx])))
         else:
-                image = Image.open(os.path.join(self.root_dir, "images", self.filenames[idx]))
+            image = Image.open(os.path.join(self.root_dir, "images", self.filenames[idx]))
         label = self.labels[idx]
-        return image, label    
-
+        return image, label
 
 
 class PreActBasicBlock(nn.Module):
@@ -96,6 +97,7 @@ class PreActBasicBlock(nn.Module):
         out += self.shortcut(x)
         return out
 
+
 class BasicBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1):
         super(BasicBlock, self).__init__()
@@ -108,7 +110,10 @@ class BasicBlock(nn.Module):
         self.bn2 = nn.BatchNorm2d(out_channels)
 
         if stride != 1 or in_channels != out_channels:
-            self.shortcut = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False)
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
         else:
             self.shortcut = nn.Identity()
 
@@ -116,49 +121,8 @@ class BasicBlock(nn.Module):
         out = self.relu(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
         out += self.shortcut(x)
+        out = self.relu(out)
         return out
-
-
-# class MyConv(nn.Module):
-#     def __init__(self, num_classes=100, base=64):
-#
-#         super().__init__()
-#
-#         # Create a feature space
-#         self.stem = nn.Conv2d(in_channels=3, out_channels=base, kernel_size=3, stride=1, padding=1, bias=False)
-#
-#         self.stage1 = self.make_stage(base, base, stride=1)  # Output: (64, 128, 128)
-#         self.stage2 = self.make_stage(base, base * 2, stride=2)  # Output: (128, 64, 64)
-#         self.stage3 = self.make_stage(base * 2, base * 4, stride=2)  # Output: (256, 32, 32)
-#         self.stage4 = self.make_stage(base * 4, base * 8, stride=2)  # Output: (512, 16, 16)
-#
-#         self.pool = nn.AdaptiveAvgPool2d((1, 1))
-#         self.fc = nn.Linear(base * 8, num_classes)
-#         self.flatten = nn.Flatten()
-#
-#         self._init_identity_last_bn()
-#
-#     def make_stage(self, in_channels, out_channels, stride=1):
-#         return nn.Sequential(
-#             PreActBasicBlock(in_channels, out_channels, stride),
-#             PreActBasicBlock(out_channels, out_channels, stride=1)
-#         )
-#
-#     def _init_identity_last_bn(self):
-#         # Zero-init the LAST BN gamma in each block
-#         for m in self.modules():
-#             if isinstance(m, PreActBasicBlock):
-#                 nn.init.zeros_(m.bn2.weight)
-#
-#     def forward(self, x, return_intermediate=False):
-#         x = self.stem(x)
-#         x = self.stage1(x)
-#         x = self.stage2(x)
-#         x = self.stage3(x)
-#         x = self.stage4(x)
-#         x = self.flatten(self.pool(x))
-#         x = self.fc(x)
-#         return x
 
 
 class MyConv(nn.Module):
@@ -167,58 +131,92 @@ class MyConv(nn.Module):
         super().__init__()
 
         # Create a feature space
-        self.stem = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=base, kernel_size=7, stride=1, padding=3, bias=False),
-            nn.BatchNorm2d(base),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        )
+        self.stem = nn.Conv2d(in_channels=3, out_channels=base, kernel_size=3, stride=1, padding=1, bias=False)
 
-        self.res_block1 = BasicBlock(base, base, stride=2)
-        self.res_block2 = BasicBlock(base, base * 2, stride=1)
+        self.stage1 = self.make_stage(base, base, stride=1)  # Output: (64, 128, 128)
+        self.stage2 = self.make_stage(base, base * 2, stride=2)  # Output: (128, 64, 64)
+        self.stage3 = self.make_stage(base * 2, base * 4, stride=2)  # Output: (256, 32, 32)
+        self.stage4 = self.make_stage(base * 4, base * 8, stride=2)  # Output: (512, 16, 16)
 
-        self.res_block3 = BasicBlock(base * 2, base * 2, stride=1)
-        self.res_block4 = BasicBlock(base * 2, base * 2, stride=2)
+        self.pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(base * 8, num_classes)
+        self.flatten = nn.Flatten()
 
-        self.res_block5 = BasicBlock(base * 2, base * 4, stride=1)
-        self.res_block6 = BasicBlock(base * 4, base * 4, stride=2)
-
-        self.res_block7 = BasicBlock(base * 4, base * 8, stride=1)
-        self.res_block8 = BasicBlock(base * 8, base * 8, stride=2)
-
-        self.classifier = nn.Sequential(
-            nn.AdaptiveAvgPool2d(1),
-            nn.Flatten(),
-            nn.ReLU(),
-            nn.Dropout(0.1),
-            nn.Linear(base * 8, num_classes)
-        )
+        # self._init_identity_last_bn()
 
     def make_stage(self, in_channels, out_channels, stride=1):
         return nn.Sequential(
-            BasicBlock(in_channels, out_channels, stride),
-            BasicBlock(out_channels, out_channels, stride=1)
+            PreActBasicBlock(in_channels, out_channels, stride),
+            PreActBasicBlock(out_channels, out_channels, stride=1)
         )
 
+    def _init_identity_last_bn(self):
+        # Zero-init the LAST BN gamma in each block
+        for m in self.modules():
+            if isinstance(m, PreActBasicBlock):
+                nn.init.zeros_(m.bn2.weight)
+
     def forward(self, x, return_intermediate=False):
-        x = self.stem(x)  # Output: (64, 64, 64)
-
-        x = self.res_block1(x)  # Output: (64, 32, 32)
-        x = self.res_block2(x)  # Output: (128, 32, 32)
-
-        x = self.res_block3(x)  # Output: (128, 32, 32)
-        x = self.res_block4(x)  # Output: (128, 16, 16)
-
-        x = self.res_block5(x)  # Output: (256, 16, 16)
-        x = self.res_block6(x)  # Output: (256, 8, 8)
-
-        x = self.res_block7(x)  # Output: (512, 8, 8)
-        x = self.res_block8(x)  # Output: (512, 4, 4)
-
-        x = self.classifier(x)  # Output: (num_classes, 1, 1)
+        x = self.stem(x)
+        x = self.stage1(x)
+        x = self.stage2(x)
+        x = self.stage3(x)
+        x = self.stage4(x)
+        x = self.flatten(self.pool(x))
+        x = self.fc(x)
         return x
 
-    
+
+# class MyConv(nn.Module):
+#     def __init__(self, num_classes=100, base=64):
+#
+#         super().__init__()
+#
+#         # Create a feature space
+#         self.stem = nn.Sequential(
+#             nn.Conv2d(in_channels=3, out_channels=base, kernel_size=7, stride=1, padding=3, bias=False),
+#             nn.BatchNorm2d(base),
+#             nn.ReLU(inplace=True),
+#         )
+#
+#         self.stage1 = self.make_stage(base, base, stride=2)
+#         self.stage2 = self.make_stage(base, base * 2, stride=2)
+#         self.stage3 = self.make_stage(base * 2, base * 4, stride=2)
+#         self.stage4 = self.make_stage(base * 4, base * 8, stride=2)
+#
+#         self.classifier = nn.Sequential(
+#             nn.AdaptiveAvgPool2d(1),
+#             nn.Flatten(),
+#             nn.Dropout(0.3),
+#             nn.Linear(base * 8, num_classes)
+#         )
+#
+#         # self._init_last_bn()
+#
+#     def make_stage(self, in_channels, out_channels, stride=1):
+#         return nn.Sequential(
+#             BasicBlock(in_channels, out_channels, stride=stride),
+#             BasicBlock(out_channels, out_channels, stride=1)
+#         )
+#
+#     def _init_last_bn(self):
+#         # Zero-init the LAST BN gamma in each block
+#         for m in self.modules():
+#             if isinstance(m, BasicBlock):
+#                 nn.init.zeros_(m.bn2.weight)
+#
+#     def forward(self, x, return_intermediate=False):
+#         x = self.stem(x)  # Output: (64, 128, 128)
+#
+#         x = self.stage1(x)  # Output: (64, 64, 64)
+#         x = self.stage2(x)  # Output: (128, 32, 32)
+#         x = self.stage3(x)  # Output: (256, 16, 16)
+#         x = self.stage4(x)  # Output: (512, 8, 8)
+#
+#         x = self.classifier(x)  # Output: (num_classes, 1, 1)
+#         return x
+
+
 def evaluate(model, test_loader, criterion, device):
     """
     Evaluate the CNN classifier on the validation set.
@@ -233,7 +231,7 @@ def evaluate(model, test_loader, criterion, device):
         float: Average loss on the test set.
         float: Accuracy on the test set.
     """
-    model.eval() # Set model to evaluation mode
+    model.eval()  # Set model to evaluation mode
 
     with torch.no_grad():
         total_loss = 0.0
@@ -254,16 +252,16 @@ def evaluate(model, test_loader, criterion, device):
             _, predictions = torch.max(logits, dim=1)
             num_correct += (predictions == labels).sum().item()
             num_samples += len(inputs)
-            
 
     # Evaluate the model on the validation set
     avg_loss = total_loss / len(test_loader)
     accuracy = num_correct / num_samples
-    
+
     return avg_loss, accuracy
 
-def train(model, train_loader, val_loader, optimizer, criterion, device,
-          num_epochs):
+
+def train(model, train_loader, val_loader, optimizer, criterion,
+          device, num_epochs, filename, scheduler=False, warmup_epochs=0):
     """
     Train the CNN classifer on the training set and evaluate it on the validation set every epoch.
 
@@ -279,11 +277,17 @@ def train(model, train_loader, val_loader, optimizer, criterion, device,
 
     # Place the model on device
     model = model.to(device)
+
+    if scheduler:
+        warmup = LinearLR(optimizer, start_factor=1 / warmup_epochs, total_iters=warmup_epochs)
+        cosine = CosineAnnealingLR(optimizer, T_max=num_epochs - warmup_epochs, eta_min=0.0)
+        scheduler = SequentialLR(optimizer, schedulers=[warmup, cosine], milestones=[warmup_epochs])
+
     for epoch in range(num_epochs):
-        model.train() # Set model to training mode
+        model.train()  # Set model to training mode
 
         with tqdm(total=len(train_loader),
-                  desc=f'Epoch {epoch +1}/{num_epochs}',
+                  desc=f'Epoch {epoch + 1}/{num_epochs}',
                   position=0,
                   leave=True) as pbar:
             for inputs, labels in train_loader:
@@ -308,13 +312,25 @@ def train(model, train_loader, val_loader, optimizer, criterion, device,
                 pbar.update(1)
                 pbar.set_postfix(loss=loss.item())
 
-            avg_loss, accuracy = evaluate(model, val_loader, criterion, device)
-            results = f'Validation set: Average loss = {avg_loss:.4f}, Accuracy = {accuracy:.4f}'
-            print(results)
+        # Optimize the learning rate based on the schedule if it exists
+        if scheduler:
+            scheduler.step()
 
-            # Write to log file
-            with open('training_log_9_AdamW.txt', 'a') as f:
-                f.write(results + '\n')
+        train_loss, train_acc = evaluate(model, train_loader, criterion, device)
+        val_loss, val_acc = evaluate(model, val_loader, criterion, device)
+        results = f'''[{epoch + 1}/{num_epochs}]:
+        Training set: Average loss = {train_loss:.4f}, Accuracy = {train_acc:.4f}
+        Validation set: Average loss = {val_loss:.4f}, Accuracy = {val_acc:.4f}
+        '''
+        #     f'[{epoch + 1}/{num_epochs}]:\n'
+        #     f'Training set: Average loss = {train_loss:.4f}, Accuracy = {train_acc:.4f}\n'
+        #     f'Validation set: Average loss = {val_loss:.4f}, Accuracy = {val_acc:.4f}'
+        # )
+        print(results)
+
+        # Write to log file
+        with open(filename, 'a') as f:
+            f.write(results + '\n')
 
 
 def test(model, test_loader, device):
@@ -331,7 +347,7 @@ def test(model, test_loader, device):
         float: Accuracy on the test set.
     """
     model = model.to(device)
-    model.eval() # Set model to evaluation mode
+    model.eval()  # Set model to evaluation mode
 
     with torch.no_grad():
         all_preds = []
@@ -346,66 +362,65 @@ def test(model, test_loader, device):
             preds = list(zip(labels, predictions.tolist()))
             all_preds.extend(preds)
     return all_preds
-            
-            
 
-    # Evaluate the model on the validation set
-    avg_loss = total_loss / len(test_loader)
-    accuracy = num_correct / num_samples
-    
-    return avg_loss, accuracy
+    # # Evaluate the model on the validation set
+    # avg_loss = total_loss / len(test_loader)
+    # accuracy = num_correct / num_samples
+    #
+    # return avg_loss, accuracy
+
 
 def main(args):
     base = 64
-    num_epochs = 50
+    num_epochs = 70
 
     image_net_mean = torch.Tensor([0.485, 0.456, 0.406])
     image_net_std = torch.Tensor([0.229, 0.224, 0.225])
-    
+
     ## Define data transformation
-    # train_transform = transforms.Compose([
-    #     transforms.RandomHorizontalFlip(p=0.5),
-    #     transforms.RandomRotation(degrees=15),
-    #     transforms.Resize((128, 128)),
-    #     transforms.ToTensor(),
-    #     transforms.Normalize(image_net_mean, image_net_std),
-    # ])
-    #
-    # val_transform = transforms.Compose([
-    #     transforms.Resize((128, 128)),
-    #     transforms.ToTensor(),
-    #     transforms.Normalize(image_net_mean, image_net_std),
-    # ])
-
     train_transform = transforms.Compose([
-        # geometric & scale
-        transforms.RandomResizedCrop(128, scale=(0.7, 1.0), ratio=(0.9, 1.1)),
-        transforms.RandomHorizontalFlip(p=0.5),
-
-        # photometric
-        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.02),
-        transforms.RandomGrayscale(p=0.05),
-        transforms.RandomPerspective(distortion_scale=0.15, p=0.1),
-        transforms.RandomRotation(degrees=10),
-
-        # to tensor + normalize
+        transforms.RandomResizedCrop(128, scale=(0.6, 1.0), ratio=(3 / 4, 4 / 3)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ColorJitter(0.2, 0.2, 0.2, 0.1),
         transforms.ToTensor(),
         transforms.Normalize(image_net_mean, image_net_std),
-
-        # cutout-like reg (must be AFTER ToTensor)
-        transforms.RandomErasing(p=0.25, scale=(0.02, 0.12), ratio=(0.3, 3.3), inplace=True),
     ])
 
     val_transform = transforms.Compose([
-        transforms.Resize(144),  # slightly bigger
-        transforms.CenterCrop(128),  # deterministic eval crop
+        transforms.Resize(144),
+        transforms.CenterCrop(128),
         transforms.ToTensor(),
         transforms.Normalize(image_net_mean, image_net_std),
     ])
 
+    # train_transform = transforms.Compose([
+    #     # geometric & scale
+    #     transforms.RandomResizedCrop(128, scale=(0.7, 1.0), ratio=(0.9, 1.1)),
+    #     transforms.RandomHorizontalFlip(p=0.5),
+    #
+    #     # photometric
+    #     transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.02),
+    #     transforms.RandomGrayscale(p=0.05),
+    #     transforms.RandomPerspective(distortion_scale=0.15, p=0.1),
+    #     transforms.RandomRotation(degrees=10),
+    #
+    #     # to tensor + normalize
+    #     transforms.ToTensor(),
+    #     transforms.Normalize(image_net_mean, image_net_std),
+    #
+    #     # cutout-like reg (must be AFTER ToTensor)
+    #     transforms.RandomErasing(p=0.25, scale=(0.02, 0.12), ratio=(0.3, 3.3), inplace=True),
+    # ])
+    #
+    # val_transform = transforms.Compose([
+    #     transforms.Resize(144),  # slightly bigger
+    #     transforms.CenterCrop(128),  # deterministic eval crop
+    #     transforms.ToTensor(),
+    #     transforms.Normalize(image_net_mean, image_net_std),
+    # ])
 
     data_root = 'data'
-    
+
     # Create MiniPlaces dataset object
     miniplaces_train = MiniPlaces(data_root,
                                   split='train',
@@ -416,14 +431,14 @@ def main(args):
                                 label_dict=miniplaces_train.label_dict)
 
     # Create the dataloaders
-    
+
     # Define the batch size and number of workers
     batch_size = 64
     num_workers = 8
 
-    # lr = 0.05 * (batch_size / 256)
-    lr = 3e-4
-    weight_decay = 1e-4
+    lr = 0.1 * (batch_size / 256)
+    # lr = 1e-4
+    weight_decay = 5e-4  # 7e-4?
     momentum = 0.9
 
     # Create DataLoader for training and validation sets
@@ -440,45 +455,47 @@ def main(args):
     # device = torch.device('mps')
 
     model = MyConv(num_classes=len(miniplaces_train.label_dict), base=base)
-                   
-    optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
-    # optimizer = torch.optim.SGD(
-    #     model.parameters(),
-    #     lr=lr,
-    #     weight_decay=weight_decay,
-    #     momentum=momentum,
-    #     nesterov=True
-    # )
+
+    # optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
+    optimizer = torch.optim.SGD(
+        model.parameters(),
+        lr=lr,
+        weight_decay=weight_decay,
+        momentum=momentum,
+        nesterov=True
+    )
 
     criterion = nn.CrossEntropyLoss(label_smoothing=0.1)
+    filename = 'training_log_32.txt'
 
     if not args.test:
 
         train(model, train_loader, val_loader, optimizer, criterion,
-              device, num_epochs=num_epochs)
+              device, num_epochs, filename, scheduler=True, warmup_epochs=5)
 
         torch.save({'model_state_dict': model.state_dict(),
-                    'optimizer_state_dict':optimizer.state_dict()}, 'model.ckpt')
+                    'optimizer_state_dict': optimizer.state_dict()}, 'model.ckpt')
 
     else:
         miniplaces_test = MiniPlaces(data_root,
                                      split='test',
                                      transform=val_transform)
         test_loader = DataLoader(miniplaces_test,
-                                batch_size=batch_size,
-                                num_workers=num_workers,
-                                shuffle=False)        
+                                 batch_size=batch_size,
+                                 num_workers=num_workers,
+                                 shuffle=False)
         checkpoint = torch.load(args.checkpoint, weights_only=True)
         model.load_state_dict(checkpoint['model_state_dict'])
         preds = test(model, test_loader, device)
         write_predictions(preds, 'predictions.csv')
+
 
 def write_predictions(preds, filename):
     with open(filename, 'w') as f:
         writer = csv.writer(f, delimiter=',')
         for im, pred in preds:
             writer.writerow((im, pred))
-    
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
